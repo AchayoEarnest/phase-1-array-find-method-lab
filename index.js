8aa176285ac16c81cdeddefd2d424b8fb594181e
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-10 23:43:20 +0300

    commit tree 101594bcb3c70875f34b474d843426671376a34e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+superbowlWin = (array) => {
+  let result = array.find( array => array.result === "W" );
+
+  if (result) {
+    return result.year;
+  }else {
+    return undefined
+  }
+
+}
